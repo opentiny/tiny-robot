37eb3333fbc67fd5105043bf91b9dd180829dc42
Hexqi <wu_12556@126.com>
2025-04-24 12:34:38 +0800

    docs: update guide (#26)

--- docs/demos/sender/Mode.vue
+++ docs/demos/sender/Mode.vue
@@ -1,9 +1,9 @@
 <template>
   <div style="display: flex; flex-direction: column; gap: 20px">
     <h4>单行模式</h4>
-    <tr-sender />
+    <tr-sender class="chat-input" />
     <h4>多行模式</h4>
-    <tr-sender mode="multiple" />
+    <tr-sender mode="multiple" class="chat-input" />
   </div>
 </template>
 
