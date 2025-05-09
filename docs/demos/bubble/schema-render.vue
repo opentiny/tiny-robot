991deee3c256fbddcc738ce798410a1cd2730e9a
gene9831 <pacify.98@gmail.com>
2025-05-09 18:22:44 +0800

    feat: add schema card and render components with documentation updates

---
+++ docs/demos/bubble/schema-render.vue
@@ -0,0 +1,53 @@
+<template>
+  <div style="display: flex; flex-direction: column; gap: 16px">
+    <label>使用插槽渲染运行时渲染</label>
+    <tr-bubble :avatar="aiAvatar">
+      <schema-card :schema="schemaObj"></schema-card>
+    </tr-bubble>
+
+    <label>使用markdown渲染运行时渲染（webcomponent）</label>
+    <tr-bubble :avatar="aiAvatar" type="markdown" :content="mdContent" :mdConfig="{ html: true }"></tr-bubble>
+  </div>
+</template>
+
+<script setup lang="tsx">
+// import { TrBubble } from '@opentiny/tiny-robot'
+import { IconAi } from '@opentiny/tiny-robot-svgs'
+import { defineCustomElement, h, ref } from 'vue'
+import SchemaCard from './schema-card.vue'
+
+const aiAvatar = h(IconAi, { style: { fontSize: '32px' } })
+
+const schemaObj = ref(
+  JSON.stringify({
+    state: {},
+    methods: {},
+    componentName: 'Page',
+    props: {},
+    children: [
+      { componentName: 'Text', props: { text: '运行时渲染器文本' } },
+      { componentName: 'Button', props: { text: '运行时渲染器按钮' } },
+    ],
+  }),
+)
+
+// 下面的代码应放在应用挂载前执行
+if (!customElements.get('schema-card')) {
+  // 将 Vue 组件转为自定义元素类。
+  const CardElement = defineCustomElement(SchemaCard)
+  // 在浏览器中注册元素类。
+  customElements.define('schema-card', CardElement)
+}
+
+const mdContent = `# h1 Heading
+## Emphasis
+
+**This is bold text**
+
+*This is italic text*
+
+_This is italic text_
+
+<schema-card schema='${schemaObj.value}'></schema-card>
+`
+</script>
