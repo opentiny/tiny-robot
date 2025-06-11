35de46ff3fd4ddc0c88ddaa4f9de74c4b45a8b14
SonyLeo <746591437@qq.com>
2025-06-10 18:50:19 -0700

    fix: remove demo case

--- packages/vite-project/src/main.ts
+++
@@ -1,9 +0,0 @@
-import { createApp, defineCustomElement } from 'vue'
-import './style.css'
-// import '@opentiny/tiny-robot/dist/style.css'
-import App from './App.vue'
-import SenderShadow from './components/sender.ce.vue'
-
-customElements.define('sender-shadow', defineCustomElement(SenderShadow))
-
-createApp(App).mount('#app')
