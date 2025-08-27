76922e2f0dc2aa540541abc15aab686798c6554a
SonyLeo <746591437@qq.com>
2025-08-27 00:14:21 -0700

    fix: review suggestion

--- packages/test/vite.config.ts
+++ packages/test/vite.config.ts
@@ -1,17 +1,11 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import { resolve } from 'path'
 
 export default defineConfig({
   plugins: [vue()],
   server: {
     port: 3333,
     host: true,
-  },
-  resolve: {
-    alias: {
-      '@': resolve(__dirname, './src'),
-    },
   },
   build: {
     outDir: 'dist',
